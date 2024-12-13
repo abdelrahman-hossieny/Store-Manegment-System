bcb3364ebe30d54928127f4f40bbbe253600a204
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2024-12-13 16:23:09 +0200

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,49 @@
+package org.example.marketdemo;
+
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+    private String category;
+
+    // Constructor
+    public Product(String name, double price, int quantity, String category) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
